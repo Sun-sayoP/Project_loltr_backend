cc810a14941659361aafa59ff86ceb9d33a69538
sayoP <swj7852@gmail.com>
2022-11-13 17:22:11 +0900

    code cleanup

--- Loltr.py
+++ Loltr.py
@@ -10,6 +10,8 @@ from flask_cors import CORS
 from flask import make_response
 
 app = Flask(__name__, template_folder="templates")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 monthrate = {
